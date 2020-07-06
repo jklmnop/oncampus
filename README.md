5af58007a9088d7ff9a28332e753138bd8db4ab3
Jason Kühn <spaceyraygun@gmail.com>
2020-07-06 07:07:02 -0400

    Create a calendar system

---
+++ README.md
@@ -0,0 +1,13 @@
+```
+$ lando start
+
+$ lando composer install
+
+$ lando console doctrine:migrations:migrate
+
+// install npm modules
+$ yarn start
+
+// webpack encore watcher to build assets
+$ yarn encore dev --watch
+```
---
+++ assets/js/calendar.js
@@ -0,0 +1,48 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+import '@fullcalendar/common/main.css';
+
+import { Calendar } from '@fullcalendar/core';
+import listPlugin from '@fullcalendar/list';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const calendarEl = document.getElementById('calendar');
+
+    const calendar = new Calendar(calendarEl, {
+        plugins: [listPlugin],
+        timeZone: 'America/New_York',
+        initialView: 'listDay',
+
+        views: {
+            listDay: { buttonText: 'Day' },
+            listWeek: { buttonText: 'Week' },
+            listMonth: { buttonText: 'Month' }
+        },
+
+        headerToolbar: {
+            left: 'title',
+            center: '',
+            right: 'listDay,listWeek,listMonth'
+        },
+        eventSources: [
+            {
+                url: '/fc-load-events',
+                method: "GET",
+                extraParams: {
+                    filters: JSON.stringify({})
+                },
+                failure: () => {
+                    // alert("There was an error while fetching FullCalendar!");
+                },
+            },
+        ],
+        editable: true
+    });
+
+    calendar.render();
+});
