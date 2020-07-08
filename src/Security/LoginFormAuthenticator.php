072f61acdde2023977ddf4a0487e2f1b99e4671b
Jason Kühn <spaceyraygun@gmail.com>
2020-07-08 12:23:03 -0400

    Get basic login to work

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -75,10 +75,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // Check the user's password or other credentials and return true or false
-        // If there are no credentials to check, you can just return true
-//        throw new \Exception('TODO: check the credentials inside '.__FILE__);
-      return true;
+        return true;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
